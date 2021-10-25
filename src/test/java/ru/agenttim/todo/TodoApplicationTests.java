868166ba68f53b240f59c8d401d6fb58b6783ea1
agent_tim <agent_tim@mail.ru>
2021-10-25 22:23:23 +0500

    Initial commit

---
+++ src/test/java/ru/agenttim/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.agenttim.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
